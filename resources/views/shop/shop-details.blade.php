c04549bf8798da82244849e0bfeb971167454265
Abraham Eguiarte <abraham.lopez5085@alumnos.udg.mx>
2022-05-20 00:34:02 -0500

    Se terminó de completar el layout para las distintas vistas de la tienda

--- resources/views/shop/shop-details.blade.php
+++ resources/views/shop/shop-details.blade.php
@@ -1,118 +1,14 @@
-<!DOCTYPE html>
-<html lang="zxx">
+@extends('layouts.master')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="description" content="Male_Fashion Template">
-    <meta name="keywords" content="Male_Fashion, unica, creative, html">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Male-Fashion | Template</title>
+@section('title')
+    KiutStore | Detalles 
+@endsection
 
-    <!-- Google Font -->
-    <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;400;600;700;800;900&display=swap"
-    rel="stylesheet">
+@section('content')
 
-    <!-- Css Styles -->
-    <link rel="stylesheet" href="{{ asset('/css/bootstrap.min.css') }}" type="text/css">
-    <link rel="stylesheet" href="{{ asset('/css/font-awesome.min.css') }}" type="text/css"> 
-    <link rel="stylesheet" href="{{ asset('/css/elegant-icons.css') }}" type="text/css"> 
-    <link rel="stylesheet" href="{{ asset('/css/magnific-popup.css') }}" type="text/css"> 
-    <link rel="stylesheet" href="{{ asset('/css/nice-select.css') }}" type="text/css"> 
-    <link rel="stylesheet" href="{{ asset('/css/owl.carousel.min.css') }}" type="text/css"> 
-    <link rel="stylesheet" href="{{ asset('/css/slicknav.min.css') }}" type="text/css"> 
-    <link rel="stylesheet" href="{{ asset('/css/style.css') }}" type="text/css"> 
-</head>
-
-<body>
-    <!-- Page Preloder -->
-    <div id="preloder">
-        <div class="loader"></div>
-    </div>
-
-    <!-- Offcanvas Menu Begin -->
-    <div class="offcanvas-menu-overlay"></div>
-    <div class="offcanvas-menu-wrapper">
-        <div class="offcanvas__option">
-            <div class="offcanvas__links">
-                <a href="{{ route('login') }}">Iniciar sesión</a>
-                <a href="{{ route('register') }}">Registrarse</a>                
-            </div>
-        </div>
-        <div class="offcanvas__nav__option">
-            <a href="#" class="search-switch"><img src="../img/icon/search.png" alt=""></a>
-            <a href="#"><img src="img/icon/heart.png" alt=""></a>
-            <a href="#"><img src="img/icon/cart.png" alt=""> <span>0</span></a>
-            <div class="price">$0.00</div>
-        </div>
-        <div id="mobile-menu-wrap"></div>
-    </div>
-    <!-- Offcanvas Menu End -->
-
-    <!-- Header Section Begin -->
-    <header class="header">
-        <div class="header__top">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-12 col-md-5">
-                        <div class="header__top__right">
-                            <div class="header__top__links">
-                                @if (!Auth::check())
-                                    <a href="{{ route('login') }}">Iniciar sesión</a>
-                                    <a href="{{ route('register') }}">Registrarse</a>
-                                @else
-                                    <form method="POST" action="{{ route('logout') }}">
-                                        @csrf
-                                        <a href="{{ route('logout') }}" onclick="event.preventDefault();
-                                        this.closest('form').submit(); " role="button">{{ __('Cerrar sesión') }}
-                                        </a>
-                                    </form>
-                                    @if (Auth::user()->role===1)
-                                        <a href="{{route('productos.index')}}">Ir al CRUD</a>
-                                    @endif
-                                @endif
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-3 col-md-3">
-                    <div class="header__logo">
-                        <a href="{{redirect('/')}}"><img src="img/logoKiut.png" alt=""></a>
-                    </div>
-                </div>
-                <div class="col-lg-6 col-md-6">
-                    <nav class="header__menu mobile-menu">
-                        <ul>
-                            <li class="active"><a href="route{{'landingpage'}}">Inicio</a></li>
-                            <li><a href="{{route('shop.index')}}">Tienda</a></li>
-                            <li><a href="{{redirect('/')}}">Páginas</a>
-                                <ul class="dropdown">
-                                    <li><a href="{{redirect('/')}}">Nosotros</a></li>
-                                    <li><a href="{{redirect('/')}}">Detalles de compra</a></li>
-                                    <li><a href="{{route('cart.index')}}">Carro de compra</a></li>
-                                    <li><a href="{{redirect('/')}}">Pagar</a></li>
-                                </ul>
-                            </li>
-                        </ul>
-                    </nav>
-                </div>
-                <div class="col-lg-3 col-md-3">
-                    <div class="header__nav__option">
-                        <a href="{{redirect('/')}}" class="search-switch"><img src="../img/icon/search.png" alt=""></a>
-                        <a href="{{redirect('/')}}"><img src="../img/icon/heart.png" alt=""></a>
-                        <a href="{{redirect('/')}}"><img src="../img/icon/cart.png" alt=""> <span>0</span></a>
-                        <!-- <div class="price">$0.00</div> -->
-                    </div>
-                </div>
-            </div>
-            <div class="canvas__open"><i class="fa fa-bars"></i></div>
-        </div>
-    </header>
-    <!-- Header Section End -->
+    @section('cart')
+        <div class="price">$0.00</div>
+    @endsection
 
     <!-- Shop Details Section Begin -->
     <section class="shop-details">
@@ -121,8 +17,6 @@
                 <div class="row">
                     <div class="col-lg-12">
                         <div class="product__details__breadcrumb">
-                            <!-- <a href="./">Home</a>
-                            <a href="./shop">Shop</a> -->
                             <a href="{{route('landingpage')}}">Inicio</a>
                             <a href="{{route('shop.index')}}">Tienda</a>
                             <span>Product Details</span>
@@ -131,19 +25,7 @@
                 </div>
                 <div class="row">
                     <div class="col-lg-3 col-md-3">
-                        <ul class="nav nav-tabs" role="tablist">
-                            <!-- <li class="nav-item">
-                                <a class="nav-link active" data-toggle="tab" href="#tabs-1" role="tab">
-                                    <div class="product__thumb__pic set-bg" data-setbg="{{$product->image}}">
-                                    </div>
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" data-toggle="tab" href="#tabs-2" role="tab">
-                                    <div class="product__thumb__pic set-bg" data-setbg="{{$product->image}}">
-                                    </div>
-                                </a>
-                            </li>       -->                      
+                        <ul class="nav nav-tabs" role="tablist">                     
                         </ul>
                     </div>
                     <div class="col-lg-6 col-md-9">
@@ -178,41 +60,6 @@
                                 <span> - 5 Reviews</span>
                             </div>
                             <h3>${{$product->price}}</h3>
-                            {{-- <div class="product__details__option">
-                                <div class="product__details__option__size">
-                                    <span>Size:</span>
-                                    <label for="xxl">xxl
-                                        <input type="radio" id="xxl">
-                                    </label>
-                                    <label class="active" for="xl">xl
-                                        <input type="radio" id="xl">
-                                    </label>
-                                    <label for="l">l
-                                        <input type="radio" id="l">
-                                    </label>
-                                    <label for="sm">s
-                                        <input type="radio" id="sm">
-                                    </label>
-                                </div>
-                                <div class="product__details__option__color">
-                                    <span>Color:</span>
-                                    <label class="c-1" for="sp-1">
-                                        <input type="radio" id="sp-1">
-                                    </label>
-                                    <label class="c-2" for="sp-2">
-                                        <input type="radio" id="sp-2">
-                                    </label>
-                                    <label class="c-3" for="sp-3">
-                                        <input type="radio" id="sp-3">
-                                    </label>
-                                    <label class="c-4" for="sp-4">
-                                        <input type="radio" id="sp-4">
-                                    </label>
-                                    <label class="c-9" for="sp-9">
-                                        <input type="radio" id="sp-9">
-                                    </label>
-                                </div>
-                            </div> --}}
                             <div class="product__details__cart__option">
                                 <div class="quantity">
                                     <div class="pro-qty">
@@ -245,9 +92,6 @@
                                 <li class="nav-item">
                                     <a class="nav-link" data-toggle="tab" href="#tabs-6" role="tab">Opiniones</a>
                                 </li>
-                                {{-- <li class="nav-item">
-                                    <a class="nav-link" data-toggle="tab" href="#tabs-7" role="tab">Información adicional</a>
-                                </li> --}}
                             </ul>
                             <div class="tab-content">
                                 <div class="tab-pane active" id="tabs-5" role="tabpanel">
@@ -262,19 +106,7 @@
                                     <div class="product__details__tab__content">
                                         <div class="product__details__tab__content__item">
                                             <h5>Opiniones</h5>
-                                            <p></p>
-                                            <p></p>
                                         </div>
-                                        {{-- <div class="product__details__tab__content__item">
-                                            <h5>Material used</h5>
-                                            <p>Polyester is deemed lower quality due to its none natural quality’s. Made
-                                                from synthetic materials, not natural like wool. Polyester suits become
-                                                creased easily and are known for not being breathable. Polyester suits
-                                                tend to have a shine to them compared to wool and cotton suits, this can
-                                                make the suit look cheap. The texture of velvet is luxurious and
-                                                breathable. Velvet is a great choice for dinner party jacket and can be
-                                            worn all year round.</p>
-                                        </div> --}}
                                     </div>
                                 </div>
                                 <div class="tab-pane" id="tabs-7" role="tabpanel">
@@ -472,91 +304,4 @@
     </section>
     <!-- Related Section End -->
 
-    <!-- Footer Section Begin -->
-    <footer class="footer">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-3 col-md-6 col-sm-6">
-                    <div class="footer__about">
-                        <div class="footer__logo">
-                            <a href="{{redirect('/')}}"><img src="img/logo-footer.png" alt=""></a>
-                        </div>
-                        <p>El cliente está en el corazón de nuestro modelo de negocio único, que incluye el diseño.</p>
-                        <a href="{{redirect('/')}}"><img src="img/payment.png" alt=""></a>
-                    </div>
-                </div>
-                <div class="col-lg-2 offset-lg-1 col-md-3 col-sm-6">
-                    <div class="footer__widget">
-                        <h6>Shopping</h6>
-                        <ul>
-                            <li><a href="{{redirect('/')}}">Tienda</a></li>
-                            <li><a href="{{redirect('/')}}">Accesorios</a></li>
-                            <li><a href="{{redirect('/')}}">Rebajas</a></li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="col-lg-2 col-md-3 col-sm-6">
-                    <div class="footer__widget">
-                        <h6>Tienda</h6>
-                        <ul>
-                            <li><a href="{{redirect('/')}}">CONTACTO</a></li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="col-lg-3 offset-lg-1 col-md-6 col-sm-6">
-                    <div class="footer__widget">
-                        <h6>NewLetter</h6>
-                        <div class="footer__newslatter">
-                            <p>SUSCRÍBETE PARA RECIBIR OFERTAS EXCLUSIVAS, PROMOCIONES Y NOTICIAS DE KIUTSTORE</p>
-                            <form action="#">
-                                <input type="text" placeholder="Your email">
-                                <button type="submit"><span class="icon_mail_alt"></span></button>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-lg-12 text-center">
-                    <div class="footer__copyright__text">
-                        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                        <p>Copyright ©
-                            <script>
-                                document.write(new Date().getFullYear());
-                            </script>2020
-                            All rights reserved | This template is made with <i class="fa fa-heart-o"
-                            aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-                        </p>
-                        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                    </div>
-                </div>
-            </div>
-        </div>
-    </footer>
-    <!-- Footer Section End -->
-
-    <!-- Search Begin -->
-    <div class="search-model">
-        <div class="h-100 d-flex align-items-center justify-content-center">
-            <div class="search-close-switch">+</div>
-            <form class="search-model-form">
-                <input type="text" id="search-input" placeholder="Ingrese el producto...">
-            </form>
-        </div>
-    </div>
-    <!-- Search End -->
-
-    <!-- Js Plugins -->
-    <script src="{{ asset('/js/jquery-3.3.1.min.js') }}"></script> 
-    <script src="{{ asset('/js/bootstrap.min.js') }}"></script> 
-    <script src="{{ asset('/js/jquery.nice-select.min.js') }}"></script> 
-    <script src="{{ asset('/js/jquery.nicescroll.min.js') }}"></script> 
-    <script src="{{ asset('/js/jquery.magnific-popup.min.js') }}"></script> 
-    <script src="{{ asset('/js/jquery.countdown.min.js') }}"></script> 
-    <script src="{{ asset('/js/jquery.slicknav.js') }}"></script> 
-    <script src="{{ asset('/js/mixitup.min.js') }}"></script> 
-    <script src="{{ asset('/js/owl.carousel.min.js') }}"></script> 
-    <script src="{{ asset('/js/main.js') }}"></script> 
-</body>
-
-</html>
+@endsection
--- resources/views/shop/shopping-cart.blade.php
+++ resources/views/shop/shopping-cart.blade.php
@@ -1,119 +1,14 @@
-<!DOCTYPE html>
-<html lang="zxx">
+@extends('layouts.master')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="description" content="Male_Fashion Template">
-    <meta name="keywords" content="Male_Fashion, unica, creative, html">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>KiutStore | Shopping Cart</title>
+@section('title')
+    KiutStore | Carrito 
+@endsection
 
-    <!-- Google Font -->
-    <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;400;600;700;800;900&display=swap"
-    rel="stylesheet">
+@section('content')
 
-    <!-- Css Styles -->
-    <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
-    <link rel="stylesheet" href="css/font-awesome.min.css" type="text/css">
-    <link rel="stylesheet" href="css/elegant-icons.css" type="text/css">
-    <link rel="stylesheet" href="css/magnific-popup.css" type="text/css">
-    <link rel="stylesheet" href="css/nice-select.css" type="text/css">
-    <link rel="stylesheet" href="css/owl.carousel.min.css" type="text/css">
-    <link rel="stylesheet" href="css/slicknav.min.css" type="text/css">
-    <link rel="stylesheet" href="css/style.css" type="text/css">
-</head>
-
-<body>
-    <!-- Page Preloder -->
-    <div id="preloder">
-        <div class="loader"></div>
-    </div>
-
-     <!-- Offcanvas Menu Begin -->
-     <div class="offcanvas-menu-overlay"></div>
-    <div class="offcanvas-menu-wrapper">
-        <div class="offcanvas__option">
-            <div class="offcanvas__links">
-                <a href="{{ route('login') }}">Iniciar sesión</a>
-                <a href="{{ route('register') }}">Registrarse</a>                
-            </div>
-        </div>
-        <div class="offcanvas__nav__option">
-            <a href="#" class="search-switch"><img src="../img/icon/search.png" alt=""></a>
-            <a href="#"><img src="img/icon/heart.png" alt=""></a>
-            <a href="#"><img src="img/icon/cart.png" alt=""> <span>0</span></a>
-            <div class="price">{{$cart->price}}</div>
-        </div>
-        <div id="mobile-menu-wrap"></div>
-    </div>
-    <!-- Offcanvas Menu End -->
-
-    <!-- Header Section Begin -->
-    <header class="header">
-        <div class="header__top">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-12 col-md-5">
-                        <div class="header__top__right">
-                            <div class="header__top__links">
-                                @if (!Auth::check())
-                                    <a href="{{ route('login') }}">Iniciar sesión</a>
-                                    <a href="{{ route('register') }}">Registrarse</a>
-                                @else
-                                    <form method="POST" action="{{ route('logout') }}">
-                                        @csrf
-                                        <a href="{{ route('logout') }}" onclick="event.preventDefault();
-                                        this.closest('form').submit(); " role="button">{{ __('Cerrar sesión') }}
-                                        </a>
-                                    </form>
-                                    @if (Auth::user()->role===1)
-                                        <a href="{{route('productos.index')}}">Ir al CRUD</a>
-                                    @endif
-                                @endif
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-3 col-md-3">
-                    <div class="header__logo">
-                        <a href="{{redirect('/')}}"><img src="img/logoKiut.png" alt=""></a>
-                    </div>
-                </div>
-                <div class="col-lg-6 col-md-6">
-                    <nav class="header__menu mobile-menu">
-                        <ul>
-                            <li><a href="{{route('landingpage')}}">Inicio</a></li>
-                            <li><a href="{{route('shop.index')}}">Tienda</a></li>
-                            <li class="active"><a href="{{redirect('/')}}">Páginas</a>
-                                <ul class="dropdown">
-                                    <li><a href="{{redirect('/')}}">Nosotros</a></li>
-                                    <li><a href="{{redirect('/')}}">Detalles de compra</a></li>
-                                    <li><a href="{{route('cart.index')}}">Carro de compra</a></li>
-                                    <li><a href="{{redirect('/')}}">Pagar</a></li>
-                                </ul>
-                            </li>
-                        </ul>
-                    </nav>
-                </div>
-                <div class="col-lg-3 col-md-3">
-                    <div class="header__nav__option">
-                        <a href="{{redirect('/')}}" class="search-switch"><img src="../img/icon/search.png" alt=""></a>
-                        <a href="{{redirect('/')}}"><img src="../img/icon/heart.png" alt=""></a>
-                        <a href="{{redirect('/')}}"><img src="../img/icon/cart.png" alt=""> <span>0</span></a>
-                        <!-- <div class="price">$0.00</div> -->
-                    </div>
-                </div>
-            </div>
-            <div class="canvas__open"><i class="fa fa-bars"></i></div>
-        </div>
-    </header>
-    <!-- Header Section End -->
-
+    @section('cart')
+        <div class="price">{{$cart->price}}</div>
+    @endsection
     <!-- Breadcrumb Section Begin -->
     <section class="breadcrumb-option">
         <div class="container">
@@ -203,13 +98,6 @@
                 </div>
                 <br>
                 <div class="col-lg-4">
-                    <!-- <div class="cart__discount">
-                        <h6>Discount codes</h6>
-                        <form action="#">
-                            <input type="text" placeholder="Coupon code">
-                            <button type="submit">Apply</button>
-                        </form>
-                    </div> -->
                     <div class="cart__total">
                         <h6>Cart total</h6>
                         <ul>
@@ -224,91 +112,4 @@
     </section>
     <!-- Shopping Cart Section End -->
 
-    <!-- Footer Section Begin -->
-    <footer class="footer">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-3 col-md-6 col-sm-6">
-                    <div class="footer__about">
-                        <div class="footer__logo">
-                            <a href="{{redirect('/')}}"><img src="img/logo-footer.png" alt=""></a>
-                        </div>
-                        <p>El cliente está en el corazón de nuestro modelo de negocio único, que incluye el diseño.</p>
-                        <a href="{{redirect('/')}}"><img src="img/payment.png" alt=""></a>
-                    </div>
-                </div>
-                <div class="col-lg-2 offset-lg-1 col-md-3 col-sm-6">
-                    <div class="footer__widget">
-                        <h6>Shopping</h6>
-                        <ul>
-                            <li><a href="{{redirect('/')}}">Tienda</a></li>
-                            <li><a href="{{redirect('/')}}">Accesorios</a></li>
-                            <li><a href="{{redirect('/')}}">Rebajas</a></li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="col-lg-2 col-md-3 col-sm-6">
-                    <div class="footer__widget">
-                        <h6>Tienda</h6>
-                        <ul>
-                            <li><a href="{{redirect('/')}}">CONTACTO</a></li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="col-lg-3 offset-lg-1 col-md-6 col-sm-6">
-                    <div class="footer__widget">
-                        <h6>NewLetter</h6>
-                        <div class="footer__newslatter">
-                            <p>SUSCRÍBETE PARA RECIBIR OFERTAS EXCLUSIVAS, PROMOCIONES Y NOTICIAS DE KIUTSTORE</p>
-                            <form action="#">
-                                <input type="text" placeholder="Your email">
-                                <button type="submit"><span class="icon_mail_alt"></span></button>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-lg-12 text-center">
-                    <div class="footer__copyright__text">
-                        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                        <p>Copyright ©
-                            <script>
-                                document.write(new Date().getFullYear());
-                            </script>2020
-                            All rights reserved | This template is made with <i class="fa fa-heart-o"
-                            aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-                        </p>
-                        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                    </div>
-                </div>
-            </div>
-        </div>
-    </footer>
-    <!-- Footer Section End -->
-
-    <!-- Search Begin -->
-    <div class="search-model">
-        <div class="h-100 d-flex align-items-center justify-content-center">
-            <div class="search-close-switch">+</div>
-            <form class="search-model-form">
-                <input type="text" id="search-input" placeholder="Ingrese el producto...">
-            </form>
-        </div>
-    </div>
-    <!-- Search End -->
-
-    <!-- Js Plugins -->
-    <script src="js/jquery-3.3.1.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/jquery.nice-select.min.js"></script>
-    <script src="js/jquery.nicescroll.min.js"></script>
-    <script src="js/jquery.magnific-popup.min.js"></script>
-    <script src="js/jquery.countdown.min.js"></script>
-    <script src="js/jquery.slicknav.js"></script>
-    <script src="js/mixitup.min.js"></script>
-    <script src="js/owl.carousel.min.js"></script>
-    <script src="js/main.js"></script>
-</body>
-
-</html>
+@endsection
